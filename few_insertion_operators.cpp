f18cef1a92076d3ad67825f8f9ba87850456e337
ishabajwa001 <ishabajwa001@gmail.com>
2026-06-16 21:49:18 +0500

    Create few_insertion_operators.cpp

---
+++ few_insertion_operators.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World";
+    cout<<endl;
+    cout<<"I am learning C++";
+    cout<<endl;
+    cout<<4;
+    cout<<4.643f;
+    cout<<'I';
+}
+
+/*
+    This program demonstrates multiple cout statements.
+
+    - cout is used to display output on the screen.
+    - endl moves the cursor to the next line.
+    - Each cout statement can print text, numbers, or characters.
+    - Integers, floating-point numbers, and characters can all be
+    displayed using cout.
+
+    Output:
+    Hello World
+    I am learning C++
+    44.643I
+
+    Note:
+    Since there are no spaces or endl statements between 4, 4.643f,
+    and 'I', they are printed together as 44.643I.
+*/
